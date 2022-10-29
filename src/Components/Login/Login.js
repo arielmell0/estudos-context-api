a65483f6b12c60ad908952e6c7555f0c6b26879c
arielmell0 <arielmello@rede.ulbra.br>
2022-10-29 14:59:20 -0300

    feat: fazendo o logout

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,10 +12,18 @@ const Login = () => {
         setUser(input)
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem('user')
+        setUser({
+            name: ''
+        })
+    }
+
     return (
         <div>
             <input type="text" onChange={(e) => setInput({ name: e.target.value })} />
             <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
