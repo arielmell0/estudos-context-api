7de90a45ba730d76503937375fec7083db2eabc1
arielmell0 <arielmello@rede.ulbra.br>
2022-10-29 11:19:27 -0300

    feat: exibindo nosso context no log

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,15 @@
-const App = () => (
+import React from 'react';
+import { AuthContext } from '../../providers/auth';
+
+const App = () => {
+  const { user } = React.useContext(AuthContext)
+  console.log(user)
+
+  return (
   <>
     <div>Olá mundo</div>
   </>
-)
+  )
+}
 
 export default App;
